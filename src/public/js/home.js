f9a25212014eab843a5628234129fd95f5b799ce
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-06-19 23:47:34 +0900

    Create home.js

---
+++ src/public/js/home.js
@@ -0,0 +1,12 @@
+// const socket = io();
+
+// const welcomeForm = document.querySelector("form");
+
+// const handleSubmit = async (event) => {
+//     const input = welcomeForm.querySelector("input");
+    
+// };
+
+
+
+// welcomeForm.addEventListener("submit", handleSubmit);
