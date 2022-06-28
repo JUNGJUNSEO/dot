4d34e08ff16430eaecefc30619eaa4218762846c
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-06-28 19:52:17 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,15 +2,23 @@ import http from "http";
 import { Server } from "socket.io";
 import express from "express";
 import path from "path";
+import multer from "multer";
 
 let user;
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 const app = express();
+const imgUpload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 10000000,
+  },
+});
+
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/src/views");
-// /public에 static을 해준 이유는?
+app.use("/images", express.static("images"));
 app.use("/public", express.static(__dirname + "/src/public"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -18,12 +26,18 @@ app.use(express.json());
 app.get("/", (_, res) => res.render("home"));
 app.post("/room", (req, res) => {
  
-  const camera = req.body.camera ? false : true
-  const voice = req.body.voice ? false : true
-  const {roomname, username} = req.body
-  user = username
+  const camera = req.body.camera ? false : true;
+  const voice = req.body.voice ? false : true;
+  const {roomname, username} = req.body;
+  user = username;
   return res.render("room", {roomname, username, camera, voice})
 });
+
+app.post("/room/image", imgUpload.single("image"), (req, res) => {
+  const {file: { path }} = req
+  console.log(req.file)
+  return res.status(201).json(path);
+}); 
 app.get("/*", (_, res) => res.redirect("/"));
 
 
